56123406e98e7ade941afe69686d7dda0265d790
Kamania <kamaniajoseph@gmail.com>
2023-07-14 23:36:50 +0300

    Add http package

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,29 +8,15 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
-  @override
-  void initState() {
-    super.initState();
-    print('initState function ran');
-  }
 
   @override
   Widget build(BuildContext context) {
-    print('build function ran');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text("Choose a Location"),
         centerTitle: true,
         elevation: 0,
-      ),
-      body: ElevatedButton(
-        onPressed: (){
-          setState(() {
-            counter += 1;
-          });
-        }, child: Text("counter is $counter"),
       ),
     );
   }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    // make the request
+    Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/Africa/Nairobi"));
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    //get properties from data
+    String datetime = data['utc_datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    //print(offset);
+
+    // create date time object
+     DateTime now = DateTime.parse(datetime);
+     now = now.add(Duration(hours: int.parse(offset)));
+     print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
